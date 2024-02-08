a0708aa1a63553830c57eb33f8f16e5b1c49516f
harshkhangarot07 <140736594+harshkhangarot07@users.noreply.github.com>
2024-02-08 12:26:42 +0530

    Initial commit

---
+++ models/todos/user.models.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+//this below 'USER' is used for reference
+export const User = mongoose.model('User', userSchema);
